29169cdc04c69ac3f4cb54feb5052848f0aa9849
Alana Gabriele Amorim Silva <alanagabriele43@gmail.com>
2024-07-15 22:37:36 -0300

    codigo comentado

--- 2360. Longest Cycle in a Graph/2360. Longest Cycle in a Graph.py
+++ 2360. Longest Cycle in a Graph/2360. Longest Cycle in a Graph.py
@@ -2,20 +2,20 @@ from typing import List
 
 
 def longestCycle(self, edges: List[int]) -> int:
-    comprimento = -1
-    time = 1
-    timeVisited = [0] * len(edges)
+    comprimento = -1  # tamanho do maior cliclo encontrado.
+    time = 1  # será usada para marcar o tempo de visita dos nós.
+    timeVisited = [0] * len(edges)  # lista para armazenar o tempo de visita de cada nó.
 
-    for i in range(len(edges)):
-        if timeVisited[i]:
+    for i in range(len(edges)):  # Percorre todos os nós na lista 'edges'.
+        if timeVisited[i]:  # Se o nó já foi visitado, pula para o próximo nó.
             continue
-        startTime = time
-        u = i
-        while u != -1 and not timeVisited[u]:
-            timeVisited[u] = time
-            time += 1
-            u = edges[u]  # Mover para o próximo nó
-        if u != -1 and timeVisited[u] >= startTime:
-            comprimento = max(comprimento, time - timeVisited[u])
+        startTime = time  # Armazena o tempo atual na variável 'startTime'.
+        u = i  # Inicializa a variável 'u' com o valor do índice atual 'i'.
+        while u != -1 and not timeVisited[u]:  # Continua enquanto 'u' não for -1 e 'u' ainda não foi visitado.
+            timeVisited[u] = time  # Marca o nó 'u' com o tempo atual.
+            time += 1  # Incrementa o tempo.
+            u = edges[u]  # Move para o próximo nó na lista 'edges'.
+        if u != -1 and timeVisited[u] >= startTime:  # Se 'u' não for -1 e 'timeVisited[u]' for maior ou igual a 'startTime', um ciclo foi encontrado.
+            comprimento = max(comprimento, time - timeVisited[u])  # Atualiza 'comprimento' com o comprimento do ciclo, se for maior que o valor atual de 'comprimento'.
 
-    return comprimento
+    return comprimento  # Retorna o comprimento do ciclo mais longo encontrado, ou -1 se nenhum ciclo foi encontrado.
